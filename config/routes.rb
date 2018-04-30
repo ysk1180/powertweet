063e1526017106a8cd211c9efc096e5c437f9b8d
Yusuke Miyazaki <ysk1180@gmail.com>
2018-04-30 13:46:59 +0900

    利用規約等作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'terms', to: 'terms#index', as: :terms
+  get 'privacy', to: 'privacies#index', as: :privacy
   get 'auth/:provider/callback' => 'users#create'#このpathを通して認証が行われる。
   resources :posts, only: [:new, :create, :show]
   root to: "top#index"
