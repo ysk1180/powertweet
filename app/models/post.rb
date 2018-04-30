24bf04714291fbbc2fed599c0a464282f8854a3c
Yusuke Miyazaki <ysk1180@gmail.com>
2018-04-30 22:50:54 +0900

    Twitter本文入力任意に

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-  validates :content, presence: true
   validates :content, length: { maximum: 120 }
   validates :power, presence: true
   validates :power, length: { maximum: 90 }
